fecdf315df4226f9d40b8d5b8cceeb7fd1a40d77
Rupam Shil <rupamshil111@gmail.com>
2024-10-02 23:20:23 +0530

    (feat) made product route unauthenticated

--- quickcom-server/src/routes/product.route.ts
+++ quickcom-server/src/routes/product.route.ts
@@ -1,9 +1,7 @@
 import { fetchProducts } from '../controllers/products.controller';
-import { isAuthenticated } from '../middleware/auth.middleware';
 import { AppServer } from '../utils/app.utils';
 
 const router = AppServer.getRouter();
-router.use(isAuthenticated);
 
 router.get('/', fetchProducts);
 
